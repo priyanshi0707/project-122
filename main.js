44dc3e8446c6db5a277adfaa8bf3617299ba5149
priyanshi0707 <89638179+priyanshi0707@users.noreply.github.com>
2023-04-14 15:46:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+x = 0;
+y = 0;
+
+draw_apple = "";
+
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+function start()
+{
+  document.getElementById("status").innerHTML = "System is listening please speak";  
+  recognition.start();
+} 
+ 
+recognition.onresult = function(event) {
+
+ console.log(event); 
+
+ content = event.results[0][0].transcript;
+
+    document.getElementById("status").innerHTML = "The speech has been recognized: " + content; 
+
+}
+
+function setup() {
+ screen_width = window,innerWidth;
+    screen_height=window,innerHeight;
+     canvas=createCanvas(0,150);
+    screen_height=150;
+    screen_width=150;
+}
+
+function draw() {
+  if(draw_apple == "set")
+  {
+    document.getElementById("status").innerHTML = to_number + " Apples drawn";
+    draw_apple = "";
+        {
+    radius = Math.floor(Math.random() * 100);
+    apple(x, y, radius);
+    document.getElementById("status").innerHTML = "Apple is drawn. ";
+    draw_apple = "";
+  }
+  }
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    speak_data = "";
+}
